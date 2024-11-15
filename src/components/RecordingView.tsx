0d8be0f865f478a055a0e1dd6b84e9ed49af25f8
asipB <asip.bajrami@gmail.com>
2024-11-15 14:20:31 +0100

    fix

--- src/components/RecordingView.tsx
+++ src/components/RecordingView.tsx
@@ -74,7 +74,7 @@ const ParticipantTile = React.memo(({
                     position: 'relative',
                     width: '100%',
                     paddingTop: '56.25%', // 16:9 aspect ratio
-                    border: isActiveSpeaker ? '4px solid rgba(255, 255, 255, 1)' : '4px solid rgba(255, 255, 255, 0)\'',
+                    border: isActiveSpeaker ? '4px solid rgba(255, 255, 255, 1)' : '4px solid rgba(255, 255, 255, 0)',
                     boxShadow: isActiveSpeaker ? '0 0 10px rgba(255, 255, 255, 0.3)' : '0 0 10px rgba(255, 255, 255, 0)',
                     borderRadius: '16px',
                     overflow: 'hidden',
